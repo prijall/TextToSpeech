5af94d9f56bd854600151ab71d40aeda74e6499b
prijall <135851434+prijall@users.noreply.github.com>
2023-12-17 17:00:34 +0545

    Update TextToSpecch.py

--- TextToSpecch.py
+++ TextToSpecch.py
@@ -1,9 +1,9 @@
-import pyttsx3
-import wikipedia
+import pyttsx3 #importing library that converts text to speech
+import wikipedia #importing wikipedia so we can extract info from there
 
-voice = pyttsx3.init()
-In = input("Wiki Search: ")
+voice = pyttsx3.init() #initialize text to speech engine and creates speech engine name voice
+In = input("Wiki Search: ") 
 result= wikipedia.summary(In, sentences= 3)
 print(result)
-voice.say(result)
-voice.runAndWait()
+voice.say(result) #passes the fetched summary to the text-to-speech engine to convert it into speech.
+voice.runAndWait() #executes the speech synthesis, vocalizing the summary retrieved from Wikipedia.
